3326c2b9bbe074674654718a8bf6579386fe513e
Andrei <umulazu@gmail.com>
2018-12-18 13:07:42 +0300

    starting React: handling click on <a> tag

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import Header from "./Header";
 import Footer from "./Footer";
+import Main from "./Main";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            forecastMode: 0,
+            cityId: ""
+        };
+    }
+
+    setForecastMode = (e, mode) => {
+        e.preventDefault();
+        this.setState({
+            forecastMode: mode
+        });
+        console.log(this.state);
+    };
+
     render() {
         return (
             <div className="container container-fluid">
-                <Header />
+                <Header handleMenuItem = {this.setForecastMode.bind(this)}/>
+                <Main forecastMode = {this.state.forecastMode} cityId = {this.state.cityId}/>
                 <Footer />
             </div>
         );
--- src/Header.js
+++ src/Header.js
@@ -1,39 +1,74 @@
 import React from "react";
 
-const Header = () => (
-    <div id="menu-container">
-        {/*menu*/}
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="navbar-header">
-                <a className="navbar-brand" href="../public/index.html">Погода в городе</a>
-                {/*collapsed menu*/}
-                <button className="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"/>
-                </button>
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            city: "Ярославль",
+            cityId: ""          //TODO: пробрасывать в родительский state. Не хранить тут вообще state lifting
+        };
+        this.input = React.createRef();
+    };
 
-                {/* SEARCHING FOR CITY*/}
-                <form name="form_container" className="navbar-form navbar-left">
-                    <div className="form-group">
-                        <input type="text" id="textboxCity" className="form-control" placeholder="Москва" required pattern="[А-Яа-я ]{1,15}" size="15"/>
+    handleSubmitCity = (e) => {
+        e.preventDefault();
+        let value = this.input.current.value;
+        let cities = {
+            "ярославль": "",
+            "рыбинск": "",
+            "кострома": "",
+            "вологда": "",
+            "москва": "",
+            "санкт-Петербург": ""
+        };
+
+        if (!(cities.hasOwnProperty(value.toLowerCase()))) {
+            // TODO: заменить alert чем-то красивым
+            alert("Введите известный город!");
+            return;
+        }
+        this.setState({
+            cityId: cities[value],
+            city: value.charAt(0).toUpperCase() + value.slice(1)
+        });
+    };
+
+    render() {
+
+        return (
+            <div id="menu-container">
+                {/*menu*/}
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="../public/index.html">Погода в городе {this.state.city}</a>
+                        {/*collapsed menu*/}
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                                data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"/>
+                        </button>
+
+                        {/* SEARCHING FOR CITY*/}
+                        <form name="form_container" className="navbar-form navbar-left" onSubmit={this.handleSubmitCity}>
+                            <div className="form-group">
+                                <input type="text" defaultValue={this.state.city} ref={this.input} className="form-control" required pattern="[А-Яа-я ]{1,15}" size="15"/>
+                            </div>
+                            <button type="submit" className="btn btn-default">Найти</button>
+                        </form>
                     </div>
-                    <button type="submit" className="btn btn-default">Найти</button>
-                </form>
+                    {/*Collect the nav links, forms, and other content for toggling*/}
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-item nav-link nav__mode_current-day active" onClick={this.props.handleMenuItem} href="#">Сейчас<span className="sr-only">(current)</span></a>
+                            <a className="nav-item nav-link nav__mode_3-day" href="../public/index.html">На 3 дня</a>
+                            <a className="nav-item nav-link nav__mode_5-day" href="../public/index.html">На 5 дней</a>
+                        </div>
+                    </div>
+                    {/*/.navbar-collapse*/}
+                </nav>
             </div>
-            {/*Collect the nav links, forms, and other content for toggling*/}
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div className="navbar-nav">
-                    <a className="nav-item nav-link nav__mode_current-day active" href="../public/index.html">Сегодня <span className="sr-only">(current)</span></a>
-                    <a className="nav-item nav-link nav__mode_3-day" href="../public/index.html">На 3 дня</a>
-                    <a className="nav-item nav-link nav__mode_5-day" href="../public/index.html">На 5 дней</a>
-                </div>
-            </div>
-            {/*/.navbar-collapse*/}
-        </nav>
-    </div>
-);
-
-
+        );
+    }
+}
 
 export default Header;
